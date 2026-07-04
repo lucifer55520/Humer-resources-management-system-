06ae73c00cf923c7c47ecc8efe58ecdc15e4b7be
lucifer55520 <taofficial974@gmail.com>
2026-07-04 14:10:23 +0530

    Create hrmsenums.java

---
+++ hrmsenums.java
@@ -0,0 +1,6 @@
+package com.hrms.core;
+
+public enum Role { ADMIN, EMPLOYEE }
+public enum AttendanceStatus { PRESENT, ABSENT, HALF_DAY, LEAVE }
+public enum LeaveType { PAID, SICK, UNPAID }
+public enum LeaveStatus { PENDING, APPROVED, REJECTED }
